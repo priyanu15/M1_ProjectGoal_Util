fead51a796c54a1f15d7d954916007d0617282fd
priyanu15 <priyanutushar36@gmail.com>
2022-03-03 22:36:47 +0530

    commit

---
+++ 3_Implementation/version1/taxcalculator.h
@@ -0,0 +1,18 @@
+/**
+ * @file taxcalculator.h
+ * @author Priyanu Tushar
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+//Header guard
+#ifndef taxcalculator_H_
+#define taxcalculator_H_
+
+//Include files
+#include <stdio.h> 
+//Macros
+#endif
