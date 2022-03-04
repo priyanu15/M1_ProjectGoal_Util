5a31a9c13334cfc6198a20c2cd35efde2157a5a8
priyanu15 <priyanutushar36@gmail.com>
2022-03-04 14:59:55 +0530

    Makefile for Multi OS

--- 3_Implementation/version1/taxcalculator.c
+++ 3_Implementation/version1/taxcalculator.c
@@ -9,36 +9,41 @@
  * @copyright Copyright (c) 2022
  * 
  */
-// Header File
-#include <stdio.h> 
+//Header file
+#include <stdio.h>
 
 //Main function
 int main(void)
 {
     //Declaring and initializing variabless
+    
     double income, tax;
-   
+    
     char quit = ' ';
 
     //Loop for multiple oparations
+    
     while (quit != 'q' && quit != 'Q') {
 
         //Getting input from the user
+      
         printf("\n\n\nInput your annual income:\t");
-        
+       
         scanf("%lf", &income);
 
         //Taxable income: 30 percent of the income isn't taxable
+      
         income = income * 70 / 100;
 
         //Contitions for calculating tax
+        
         if (income <= 250000) {
             
             tax = 0;                                            //No tax for 250000 or below income
         }
 
         else if (income <= 650000) {
-           
+            
             tax = (income - 250000) * 10 / 100;                 //10% tax for next 400000 income
         }
 
@@ -48,12 +53,12 @@ int main(void)
         }
 
         else if (income <= 2150000) {
-           
+            
             tax = (income - 1150000) * 20 / 100 + 115000;       //20% tax for next 1000000 income
         }
 
         else {
-           
+            
             tax = (income - 2150000) * 25 / 100 + 315000;       //25% tax for next any amount of income
         }
 
@@ -64,12 +69,12 @@ int main(void)
         //Getting out of the loop
         
         getchar();
-       
+        
         printf("Input Q or q to exit. Input any other character to continue: ");
-       
+        
         scanf("%c", &quit);
     }
 
-   
+    
     return 0;
 }
